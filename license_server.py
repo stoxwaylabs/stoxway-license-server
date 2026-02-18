6d9a7a4e328f3e4944f7d8988510703ab6116bf8
stoxwaylabs <stoxway@gmail.com>
2026-02-18 21:37:55 +0530

    Update license_server.py

--- license_server.py
+++ license_server.py
@@ -398,6 +398,11 @@ function loadLicenses() {
     </body>
     </html>
     """
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+
 
 
 
